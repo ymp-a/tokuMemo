3fa74d31cb2dbbd1cf45d8a590f111b3cdb8345a
ymp-a <hiyakashi3@gmail.com>
2022-06-27 12:29:51 +0900

    tokuMemoPJファイル追加

---
+++ tokuMemo/tokuMemo/tokuMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  tokuMemoApp.swift
+//  tokuMemo
+//
+//  Created by satoshi yamashita on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct tokuMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
