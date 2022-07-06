03e76b2f9e20391460ad90d166f785cab5c2173a
ymp-a <hiyakashi3@gmail.com>
2022-07-06 20:44:25 +0900

    tokuMemoAppのbody修正 #1

--- tokuMemo/tokuMemoApp.swift
+++ tokuMemo/tokuMemoApp.swift
@@ -13,7 +13,7 @@ struct tokuMemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TokuMemoListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
