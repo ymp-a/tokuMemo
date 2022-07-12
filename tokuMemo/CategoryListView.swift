6addf782b88aec684268712b231240aa1808d98c
ymp-a <hiyakashi3@gmail.com>
2022-07-12 22:30:36 +0900

    閉じるボタンの実装 #2

--- tokuMemo/CategoryListView.swift
+++ tokuMemo/CategoryListView.swift
@@ -15,39 +15,24 @@ struct CategoryListView: View {
                     Text("カテゴリー（大分類）")
                 } // HStackここまで
 
-                Divider()
-                    .background(Color.black)
-
                 List {
                     Text("すべて")
                     Text("日用品")
                     Text("食品")
-
                 } // Listここまで
                 .foregroundColor(.orange)
 
-                Divider()
-                    .background(Color.black)
-                HStack {
-                    VStack {
-                        Image(systemName: "house")
-                            .font(.system(size: 30))
-                        Text("Top")
-                            .font(.system(size: 12))
-                    } // VStackここまで
-                    .foregroundColor(.brown)
-                    .padding(.horizontal)
-
-                    VStack {
-                        Image(systemName: "cart")
-                            .font(.system(size: 30))
-                            .padding(.horizontal)
-                        Text("買い物リスト")
-                            .font(.system(size: 12))
-                    }
-                    .foregroundColor(.gray)
-                    Spacer()
-                } // HStackここまで
+                Button(action: {
+                    // 閉じる処理
+                }) {
+                    Text("閉じる")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .background(.orange)
+                } // 閉じるボタンここまで
+                Spacer()
             } // VStackここまで
 
             HStack {
@@ -55,20 +40,20 @@ struct CategoryListView: View {
                 VStack {
                     Button(action: {
                         // タップで画面表示させる
-                    }, label: {
+                    }) {
                         // 追加Viewへ遷移する
                         Image(systemName: "plus")
                             .font(.system(size: 24))
                             .foregroundColor(.white)
                             .background(Color.orange)
                             .clipShape(Circle())
-                    })
+                    }
                     Text("カテゴリ追加")
                         .font(.system(size: 8))
                         .foregroundColor(Color.orange)
                     Spacer()
                 } // VStackここまで
-            } //HStackここまで
+            } // HStackここまで
         } // ZStackここまで
     } // bodyここまで
 } // CategoryListViewここまで
