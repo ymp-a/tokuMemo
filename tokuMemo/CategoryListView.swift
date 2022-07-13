fa21b56452151ef63b618d2121ac4ffb7acf8d2e
ymp-a <68992872+ymp-a@users.noreply.github.com>
2022-07-13 20:49:29 +0900

    #2 カテゴリー画面の実装 (#8)
    * カテゴリー画面の実装 #2
    
    * カテゴリ追加ボタンの配置変更 #2
    
    * 閉じるボタンの実装 #2

---
+++ tokuMemo/CategoryListView.swift
@@ -0,0 +1,65 @@
+//
+//  CategoryListView.swift
+//  tokuMemo
+//
+//  Created by satoshi yamashita on 2022/07/09.
+//
+
+import SwiftUI
+
+struct CategoryListView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack(alignment: .center) {
+                    Text("カテゴリー（大分類）")
+                } // HStackここまで
+
+                List {
+                    Text("すべて")
+                    Text("日用品")
+                    Text("食品")
+                } // Listここまで
+                .foregroundColor(.orange)
+
+                Button(action: {
+                    // 閉じる処理
+                }) {
+                    Text("閉じる")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .background(.orange)
+                } // 閉じるボタンここまで
+                Spacer()
+            } // VStackここまで
+
+            HStack {
+                Spacer()
+                VStack {
+                    Button(action: {
+                        // タップで画面表示させる
+                    }) {
+                        // 追加Viewへ遷移する
+                        Image(systemName: "plus")
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                            .background(Color.orange)
+                            .clipShape(Circle())
+                    }
+                    Text("カテゴリ追加")
+                        .font(.system(size: 8))
+                        .foregroundColor(Color.orange)
+                    Spacer()
+                } // VStackここまで
+            } // HStackここまで
+        } // ZStackここまで
+    } // bodyここまで
+} // CategoryListViewここまで
+
+struct CategoryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryListView()
+    }
+}
