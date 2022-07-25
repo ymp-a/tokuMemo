424273d5bb97127b95ae428e0494dc6760fc104f
ymp-a <hiyakashi3@gmail.com>
2022-07-25 22:02:42 +0900

    商品追加画面の追加 #6

---
+++ tokuMemo/AddItemView.swift
@@ -0,0 +1,20 @@
+//
+//  AddItemView.swift
+//  tokuMemo
+//
+//  Created by satoshi yamashita on 2022/07/25.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView()
+    }
+}
