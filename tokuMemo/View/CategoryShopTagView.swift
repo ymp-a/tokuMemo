137e94c298bd057763d55324b292a386af992896
ymp-a <hiyakashi3@gmail.com>
2022-10-07 22:34:53 +0900

    ボタンの色変更,テキストのサイズ自動調節実装 #28

--- tokuMemo/View/CategoryShopTagView.swift
+++ tokuMemo/View/CategoryShopTagView.swift
@@ -24,34 +24,83 @@ struct CategoryShopTagView: View {
                 // ボタンタップでカテゴリ画面フラグオン
                 showingModalCategoryListView.toggle()
             }) {
-                Text(categoryName)
-                    .frame(maxWidth: .infinity)
-                Image(systemName: "chevron.right.circle")
+                ButtonTab(text: categoryName)
             }
             // カテゴリ画面モーダル表示
             .fullScreenCover(isPresented: $showingModalCategoryListView) {
                 CategoryListView(categoryName: $categoryName)
             }
+            .padding(5)
+            .overlay(
+                RoundedRectangle(cornerRadius: 7)
+                    .stroke(Color.orange, lineWidth: 3)
+            )
 
             Button(action: {
                 // ボタンタップでショップ画面フラグオン
                 showingModalShopListView.toggle()
             }) {
-                Text(shopName)
-                    .frame(maxWidth: .infinity)
-                Image(systemName: "chevron.right.circle")
+                ButtonTab(text: shopName)
             }
             // カテゴリ画面モーダル表示
             .fullScreenCover(isPresented: $showingModalShopListView) {
                 ShopListView(shopName: $shopName)
             }
+            .padding(5)
+            .overlay(
+                RoundedRectangle(cornerRadius: 7)
+                    .stroke(Color.orange, lineWidth: 3)
+            )
         } // HStackここまで
         .font(.title3)
-        .buttonStyle(.bordered)
+        .foregroundColor(.orange)
         .padding(.horizontal)
-
     } // bodyここまで
 } // CategoryShopTagViewここまで
+
+struct ButtonTab: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .frame(maxWidth: .infinity)
+            .bold()
+            .lineLimit(1)
+            .minimumScaleFactor(0.1)
+        Image(systemName: "chevron.right.circle")
+            .bold()
+    }
+}
+
+// struct ButtonTab2 {
+//    @State private var flag: Bool
+//    private var text: String
+//    var listView: ?
+//    var body: some View {
+//        Button(action: {
+//            // ボタンタップでカテゴリ画面フラグオン
+//            self.flag.toggle()
+//        }) {
+//          Text(text)
+//              .frame(maxWidth: .infinity)
+//              .bold()
+//              .lineLimit(1)
+//              .minimumScaleFactor(0.1)
+//          Image(systemName: "chevron.right.circle")
+//              .bold()
+//        }
+//        // カテゴリ画面モーダル表示
+//        .fullScreenCover(isPresented: $flag) {
+//            listView
+//        }
+//        .padding(5)
+//        .overlay(
+//            RoundedRectangle(cornerRadius: 7)
+//                .stroke(Color.orange, lineWidth: 3)
+//        )
+//    }
+// }
+
+//            ButtonTab2(flag: showingModalCategoryListView, text: categoryName,  listView: CategoryListView(categoryName: $categoryName))
 
 struct CategoryShopTagView_Previews: PreviewProvider {
     @State static var categoryName = "カテゴリー"
