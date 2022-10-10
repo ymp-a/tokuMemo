d89a17ee4c1feff1df82b5cd52246f57b806e8fc
ymp-a <68992872+ymp-a@users.noreply.github.com>
2022-10-10 20:52:06 +0900

    Feature/28 category shop tab buttan collor change (#37)
    * ボタンの色変更,テキストのサイズ自動調節実装 #28
    
    * ButtonAction構造体の実装 #28

--- tokuMemo/View/CategoryShopTagView.swift
+++ tokuMemo/View/CategoryShopTagView.swift
@@ -20,38 +20,63 @@ struct CategoryShopTagView: View {
     var body: some View {
 
         HStack(alignment: .center, spacing: 0) {
-            Button(action: {
-                // ボタンタップでカテゴリ画面フラグオン
-                showingModalCategoryListView.toggle()
-            }) {
-                Text(categoryName)
-                    .frame(maxWidth: .infinity)
-                Image(systemName: "chevron.right.circle")
-            }
-            // カテゴリ画面モーダル表示
-            .fullScreenCover(isPresented: $showingModalCategoryListView) {
-                CategoryListView(categoryName: $categoryName)
-            }
 
-            Button(action: {
-                // ボタンタップでショップ画面フラグオン
-                showingModalShopListView.toggle()
-            }) {
-                Text(shopName)
-                    .frame(maxWidth: .infinity)
-                Image(systemName: "chevron.right.circle")
-            }
-            // カテゴリ画面モーダル表示
-            .fullScreenCover(isPresented: $showingModalShopListView) {
-                ShopListView(shopName: $shopName)
-            }
+            ButtonAction(buttonName: $categoryName)
+            ButtonAction(buttonName: $shopName)
+
         } // HStackここまで
         .font(.title3)
-        .buttonStyle(.bordered)
+        .foregroundColor(.orange)
         .padding(.horizontal)
-
     } // bodyここまで
 } // CategoryShopTagViewここまで
+
+// Buttonの装飾
+struct ButtonTab: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .frame(maxWidth: .infinity)
+            .bold()
+            .lineLimit(1)
+            .minimumScaleFactor(0.1)
+        Image(systemName: "chevron.right.circle")
+            .bold()
+    }
+}
+
+// Buttonのふるまい
+struct ButtonAction: View {
+
+    @Binding var buttonName: String
+
+    // カテゴリー画面表示フラグ
+    @State private var showingModalListView: Bool = false
+
+    var body: some View {
+
+        Button(action: {
+            // ボタンタップでカテゴリ画面フラグオン
+            showingModalListView.toggle()
+        }) {
+            ButtonTab(text: buttonName)
+        }
+        // カテゴリ画面モーダル表示
+        .fullScreenCover(isPresented: $showingModalListView) {
+            // 引数でViewを分別
+            if buttonName == "categoryName" {
+                CategoryListView(categoryName: $buttonName)
+            } else {
+                ShopListView(shopName: $buttonName)
+            }
+        }
+        .padding(5)
+        .overlay(
+            RoundedRectangle(cornerRadius: 7)
+                .stroke(Color.orange, lineWidth: 3)
+        )
+    }
+}
 
 struct CategoryShopTagView_Previews: PreviewProvider {
     @State static var categoryName = "カテゴリー"
